c2065fc365f29c22ce019fa8f8732ca6c6e550ef
Vipul Yadav <58175102+v-ipul@users.noreply.github.com>
2025-05-12 20:27:38 +0530

    Update OcrUploader.js

--- OcrUploader.js
+++ OcrUploader.js
@@ -1,20 +1,25 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import startOcrAsyncJob from '@salesforce/apex/OCRAsyncHandler.startOcrAsyncJob';
 
 export default class OcrUploader extends LightningElement {
-    @api parentRecordId; // Used if you need to relate the OCR result to a record.
+    @api parentRecordId; // Used if the OCR result is related to a specific record
+    @track isLoading = false;
 
-    // Trigger the OCR process when file upload is finished
+    // Trigger OCR process when file upload is finished
     handleUploadFinished(event) {
+        this.isLoading = true;
         const uploadedFiles = event.detail.files;
         const documentId = uploadedFiles[0].documentId; // Get the contentDocumentId of the uploaded file
 
+        // Start the OCR job asynchronously
         startOcrAsyncJob({ contentDocumentId: documentId })
             .then(() => {
                 console.log('OCR process initiated.');
+                this.isLoading = false;
             })
             .catch(error => {
                 console.error('OCR failed:', error);
+                this.isLoading = false;
             });
     }
 }
